c5f3b042b13e3e3a009fac0932de706b6970d27b
CongSinh <sinhkute9xxx@gmail.com>
2023-03-23 16:22:30 +0700

    complete 1024px web part, add swiper-slide, header, slide item, footer

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ const heroSwiper = new Swiper(".hero__swiper", {
     clickable: true,
   },
 });
+
+const topProductSwiper = new Swiper(".top-product__swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const topProductSwiperBtn = document.querySelector("#top-product__swiper__btn");
+
+topProductSwiperBtn.querySelector(".btn-next").addEventListener("click", () => {
+  topProductSwiper.slideNext();
+});
+
+topProductSwiperBtn.querySelector(".btn-prev").addEventListener("click", () => {
+  topProductSwiper.slidePrev();
+});
